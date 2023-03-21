3b00eb7fbec6fc01efdf9a323217e535952451f7
Raquelfgb <kelkinha@hotmail.com>
2023-03-21 12:23:07 -0600

    directory change

--- lesson09/scripts/directory.js
+++ lesson09/scripts/directory.js
@@ -1,6 +1,5 @@
 /*const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
 
 // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
@@ -15,5 +14,42 @@ listbutton.addEventListener("click", showList); // example using defined functio
 function showList() {
 	display.classList.add("list");
 	display.classList.remove("grid");
-}
-*/
+}*/
+
+const requestURL = "/lesson09/card.json";
+const layouts = document.querySelector('.layouts');
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); 
+    const layouts = jsonObject['layouts'];
+    layouts.forEach(displayLayouts);
+  });
+
+function displayLayouts(layouts) {
+    let card = document.createElement("section");
+	let h2 = document.createElement("h2");
+	let logo = document.createElement("img");
+	let address = document.createElement("h3");
+	let telephone = document.createElement("h3");
+	let url = document.createElement("h3");
+
+	h2.textContent = (`${layouts.h2}`);
+    address.textContent = (`${layouts.address}`);
+    telephone.textContent = (`${layouts.telephone}`);
+	url.textContent = (`${layouts.url}`);
+
+    portrait.setAttribute('src', layouts.url);
+    portrait.setAttribute('alt', (`Logo of ${layouts.h2}`));
+    portrait.setAttribute('loading', 'lazy');
+
+    card.appendChild(h2);
+    card.appendChild(logo);
+    card.appendChild(address);
+    card.appendChild(telephone);
+	card.appendChild(url);
+
+    document.querySelector('div.layouts').appendChild(card);
+
