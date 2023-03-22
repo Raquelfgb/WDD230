14c5936b9128669bfc0929425f756ba7ede7aa49
Raquelfgb <kelkinha@hotmail.com>
2023-03-21 21:24:11 -0600

    footer

--- lesson09/scripts/directory.js
+++ lesson09/scripts/directory.js
@@ -1,20 +1,3 @@
-/*const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-
-// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
-
-gridbutton.addEventListener("click", () => {
-	// example using arrow function
-	display.classList.add("grid");
-	display.classList.remove("list");
-});
-
-listbutton.addEventListener("click", showList); // example using defined function
-
-function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
-}*/
 
 const requestURL = "/lesson09/card.json";
 const layouts = document.querySelector('.layouts');
@@ -53,3 +36,18 @@ function displayLayouts(layouts) {
 
     document.querySelector('div.layouts').appendChild(card);
 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
